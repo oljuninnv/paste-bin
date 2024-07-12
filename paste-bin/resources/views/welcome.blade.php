d60879e669d85ea0cb5fff80df35a5fc345d5dcc
Pidor <5608nik@mail.ru>
2024-07-13 01:52:11 +0700

    feat: add tailwind

--- paste-bin/resources/views/welcome.blade.php
+++ paste-bin/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        @vite('resources/css/app.css')
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -16,6 +16,9 @@
         </style>
     </head>
     <body class="antialiased">
+        <h1 class="text-3xl font-bold underline text-red-400">
+            Hello world!
+          </h1>
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
--- paste-bin/routes/web.php
+++ paste-bin/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "http_response_code()";
+    return view('welcome');
 });
